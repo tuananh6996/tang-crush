bae889db637ce51c1557ee672acf7d91b41e8e09
tuananh6996 <85301762+tuananh6996@users.noreply.github.com>
2021-06-03 22:24:22 +0700

    Update config.js

--- config.js
+++ config.js
@@ -13,7 +13,7 @@ const CONFIG = {
     btnReply: 'Gửi cho anh <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'chiều 3h anh qua đón nhé công chúa.',
+    messDesc: 'chiều 3h anh qua đón em nhé .',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'http://fb.com//m.me/ptg.beu
 }
